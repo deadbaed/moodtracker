bc32a403d6de9ffb8e6b8ce37cf7b1a19b3fad71
Philippe Loctaux <p@philippeloctaux.com>
2022-12-27 17:34:49 +0100

    added bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,31 +8,8 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="container py-4 px-3 mx-auto">
+    <h1>Hello, Bootstrap and Vite!</h1>
+    <button class="btn btn-primary" @click="count++">count is {{ count }}</button>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
