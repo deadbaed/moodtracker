69a63d359efed5b16b718b5332079351d73fa4a6
Philippe Loctaux <p@philippeloctaux.com>
2023-01-01 18:42:32 +0100

    fixed big icon

--- src/components/AddImage.vue
+++ src/components/AddImage.vue
@@ -16,7 +16,7 @@ function loadImage($event: Event) {
 <template>
   <h2>Add selfie</h2>
   <button class="btn btn-primary" onclick="document.getElementById('add-image').click();">
-    <CameraIcon class="text-white"/>
+    <CameraIcon style="width: 1.25rem; height: 1.25rem;" class="me-1" />
     Add image
   </button>
   <input class="d-none" id='add-image' type='file' accept="image/jpeg" @change="loadImage($event)"/>
