03b39d108745cfc5509013cdbc1428f3812e9c05
Philippe Loctaux <p@philippeloctaux.com>
2023-01-02 22:31:40 +0100

    added bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path";
+import {resolve} from "path";
+import {visualizer} from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
-  build: {
-    target: "esnext"
-  },
+    plugins: [
+        vue(),
+        visualizer({
+            filename: "dist/bundle-stats.html",
+            template: "treemap",
+            gzipSize: true,
+            brotliSize: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+    build: {
+        outDir: "dist/build",
+        target: "esnext",
+    },
 })
